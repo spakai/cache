0a0a199c2208eb03bc18697c9161a9854285a175
Prakash <spakai@gmail.com>
2016-01-03 15:52:31 +0800

    Added cleanup function

--- src/main/java/com/spakai/cache/CacheIt.java
+++ src/main/java/com/spakai/cache/CacheIt.java
@@ -1,6 +1,10 @@
 package com.spakai.cache;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
@@ -11,17 +15,47 @@ public class CacheIt<K, V> {
   
   private final ConcurrentMap<K, Future<V>> cache = new ConcurrentHashMap<>();
   
+  private final Map<K,Instant> lastAccessTimeKeeper = new HashMap<>();
+  
   private final ExecutorService pool; 
   
+  private final double percentageForCleanup;
+  
+  private final long maxSize;
+  
+  private volatile boolean cleanupInProgress = false;
+  
+  
+  /**
+   * Sets the size of Java Thread pool and sets default values for percentageForCleanup and maxSize.
+   * 
+   * @param numberOfThreads Number of Threads in the Thread Pool.
+   * 
+  */
   public CacheIt(int numberOfThreads) {
+    this.percentageForCleanup = 0.01;
+    this.maxSize = 10000L;
     pool = Executors.newFixedThreadPool(numberOfThreads);
   }
   
   /**
-   * Gets the value from cache if possible , otherwise runs the callable logic in a threadpool 
+   * Sets the size of Java Thread pool , values for percentageForCleanup and maxSize.
+   * 
+   * @param numberOfThreads Number of Threads in the Thread Pool.
+   * 
+  */
+  public CacheIt(int numberOfThreads, double percentage_for_cleanup, long maxSize) {
+    this.percentageForCleanup = percentage_for_cleanup;
+    this.maxSize = maxSize;
+    pool = Executors.newFixedThreadPool(numberOfThreads);
+  }
+  
+  /**
+   * Gets the value from cache if possible , otherwise runs the callable logic in a thread pool
    * and pushes the Future object into the cache. Another get() call with the same key will then get
    * the same Future object returned regardless whether the logic has completed. This ensures that the same 
-   * logic doesn't get computed twice.
+   * logic doesn't get computed twice. Whenever an item is pushed to the cache, removeLeastRecentlyUsedEntries()
+   * is called to cleanup the cache.
    * 
    * @param key Unique key to retrieve or push to cache.
    * @param callable business logic calls.
@@ -30,12 +64,40 @@ public class CacheIt<K, V> {
   public Future<V> get(K key, Callable<V> callable) {
     Future<V> future = cache.get(key);
     if (future == null) {
-      future = pool.submit(callable);
-      cache.putIfAbsent(key, future);
-    }
+    	try {
+	      future = pool.submit(callable);
+	      cache.putIfAbsent(key, future);
+	      
+	      if(!cleanupInProgress) {
+	    	  cleanupInProgress=true;
+	    	  removeLeastRecentlyUsedEntries();
+	    	  cleanupInProgress=false;
+	      }
+	      
+	    } catch(CancellationException e) {
+	    	cache.remove(key);
+	    } 
+	}
+    
+    //overwrites same key so last access time is updated
+    lastAccessTimeKeeper.put(key, Instant.now());
     
     return future;
   }
+  
+  /**
+   * Removes X least used entries from the cache. 
+   * X 's value is calculated as percentage_for_cleanup * maxSize which is defined during creation of cache
+   * On its default setting , when the cache reaches more than 10k in size , 100 LRU entries will be removed
+  */
+  
+  private void removeLeastRecentlyUsedEntries() {
+	  if(cache.size() > maxSize) {
+		  lastAccessTimeKeeper.entrySet().stream()
+		    .sorted(Map.Entry.comparingByValue())
+		    .limit((long) (percentageForCleanup * maxSize))
+		    .forEach(entry ->  cache.remove(entry.getKey()));
+	  }
+  }
 }
-
 
--- src/test/java/com/spakai/cache/CacheItTest.java
+++ src/test/java/com/spakai/cache/CacheItTest.java
@@ -11,6 +11,10 @@ import org.junit.rules.ExpectedException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CacheItTest {
   @Rule
@@ -18,11 +22,18 @@ public class CacheItTest {
   
   private CacheIt<Long, Long> cache ;
   
-  Long counter = 0L;
+  private Lock lock; 
+  
+  private Condition wasExecuted;
+  
+  Long counter;
     
   @Before
   public void setup() {
     cache = new CacheIt<Long, Long>(10);
+    lock = new ReentrantLock();
+    wasExecuted  = lock.newCondition();
+    counter = 0L;
   }
   
   @Test
@@ -30,9 +41,9 @@ public class CacheItTest {
     Future<Long> future = cache.get(1L, new Callable<Long>() {
       @Override
       public Long call() throws Exception {
-      Thread.sleep(10000); 
-      counter += 1L;
-        return counter; 
+    	  Thread.sleep(10000); 
+    	  counter += 1L;
+    	  return counter; 
       }
     });
     
